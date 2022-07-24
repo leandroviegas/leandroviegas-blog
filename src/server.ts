cafc708c2291b27a1cc0238a781243edff34aedc
Leandro Viegas <L3androViegas@gmail.com>
2022-07-24 01:44:15 -0300

    + adding cors origin to env

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:3000"],
+        origin: JSON.parse(process.env.CORS_ORIGIN),
         credentials: true,
         optionsSuccessStatus: 200,
     })
