dcf871886715026c4839531849c5068032da0b60
viKetillin <L3androViegas@gmail.com>
2022-08-13 22:22:35 -0300

    removing unused lib from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-postcss',
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-client-side-redirect`,
     {
       resolve: `gatsby-plugin-page-creator`,
       options: {
