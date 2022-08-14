ffd3828673c8c3958b1bb3ad342d65edaeb9cd40
viKetillin <L3androViegas@gmail.com>
2022-08-14 10:49:03 -0300

    trying new way to add env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.CONTENTFUL_API_URL,
+    baseURL: process.env.API_URL || "https://leandroviegasapi.onrender.com",
     withCredentials: true
 });
 
