cb0cd34143293a7d0c24cf024ca6a4ab88e0d480
Leandro Viegas <L3androViegas@gmail.com>
2023-09-15 15:28:38 -0300

    coment textarea correction

--- src/components/Forms/CommentForm.tsx
+++ src/components/Forms/CommentForm.tsx
@@ -38,7 +38,7 @@ const CommentForm = ({ post_id, referenceComment, CommentCallback, children }: C
         <div className="px-6 py-2 bg-white rounded-lg">
             {alerts["comment-errors"]?.map((message, index) => <Alert key={index} message={message} type="error" />)}
             <form onSubmit={HandleComment}>
-                <textarea value={content} onChange={e => setContent(e.target.value)} className="w-full h-12 rounded-lg resize-none py-2 text-zinc-600 outline-none" placeholder={referenceComment ? "Responder" : "Escreva um comentário"}> </textarea>
+                <textarea value={content} onChange={e => setContent(e.target.value)} className="w-full h-12 rounded-lg resize-none py-2 text-zinc-600 outline-none" placeholder={referenceComment ? "Responder" : "Escreva um comentário"} />
                 <hr />
                 <div className="my-2 flex items-center gap-4" >
                     <button type={content.trim() ? "submit" : "button"} className="text-gray-500  text-sm hover:text-gray-600 py-2 rounded-lg font-medium flex items-center gap-1"><BiPaperPlane />{referenceComment ? "Responder" : "Comentar"}</button>
