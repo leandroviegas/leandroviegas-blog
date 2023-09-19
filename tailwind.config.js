f33b5bcbcb387a07a02246c41c5091f554270cbf
Leandro Viegas <L3androViegas@gmail.com>
2023-09-19 15:17:50 -0300

    tailwind safelist config correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,13 @@ module.exports = {
   safelist: [
     {
       pattern: /(border|text)-(zinc|red|indigo|violet)-(600|700|800|200|300|400|500)/,
-    },
-    {
-      pattern: /dark:(border|text)-(zinc|red|indigo|violet)-(600|700|800|200|300|400|500)/,
+      variants: [
+        'dark',
+        'hover',
+        'focus',
+        'dark:hover',
+        'dark:focus',
+      ],
     },
     "focus:border-indigo-700",
     "focus:border-red-700"
